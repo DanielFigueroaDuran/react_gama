e97116088cfda133b00d8846093dec2a1e89e340
15819045Daniel <DanielFigueroaDuran>
2023-09-05 23:10:27 +0200

    ejercicio4 resuelto

--- src/components/buttons4/ButtonList.jsx
+++ src/components/buttons4/ButtonList.jsx
@@ -1,27 +1,53 @@
 import React from 'react'
 import './buttons.css'
 
+
+
+
 const ButtonList = ({ num }) => {
 
     let newNum = [];
 
+    function primo(numero) {
 
-    for (let i = 0; i < num; i++) {
-        //console.log(i);
-        if (num <= 1) return false;
-        for (let j = 0; j <= Math.sqrt(num); j++) {
-            if (num % i === 0) {
-                newNum.push(<button className='btn btnCousins' key={i}>{j}</button>)
+        for (let i = 2; i < numero; i++) {
+
+            if (numero % i === 0) {
+                return false;
             }
 
-
-
         }
-        // ? newNum.push(<button className='btn btnCousins' key={i}>{i}</button>)
-        // : i % 2 === 0
-        //     ? newNum.push(<button className='btn btnPar' key={i}>{i}</button>)
-        //     : newNum.push(<button className='btn btnOdd' key={i}>{i}</button>);
+
+        return numero !== 1;
     }
+
+    for (let i = 1; i <= num; i++) {
+        if (primo(i)) {
+            newNum.push(<button onClick={() => console.log(i)} key={i} className="btn btnCousins ">{i}</button>)
+        } else {
+            i % 2 === 0
+                ? newNum.push(<button onClick={() => console.log(i)} key={i} className="btn btnPar ">{i}</button>)
+                : newNum.push(<button onClick={() => console.log(i)} key={i} className="btn btnOdd ">{i}</button>);
+        }
+    }
+
+    // for (let i = 0; i < num; i++) {
+
+    //     //console.log(i);
+    //     if (num <= 1) return false;
+    //     for (let j = 0; j <= Math.sqrt(num); j++) {
+    //         if (num % i === 0) {
+    //             newNum.push(<button className='btn btnCousins' key={i}>{j}</button>)
+    //         }
+
+
+
+    //     }
+    // ? newNum.push(<button className='btn btnCousins' key={i}>{i}</button>)
+    // : i % 2 === 0
+    //     ? newNum.push(<button className='btn btnPar' key={i}>{i}</button>)
+    //     : newNum.push(<button className='btn btnOdd' key={i}>{i}</button>);
+    //}
 
     console.log(newNum);
 
